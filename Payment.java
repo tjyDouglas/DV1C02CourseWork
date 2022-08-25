61f02bccbea1f02487a7f44ce6a112d902e17612
AllanH2022 <111361757+AllanH2022@users.noreply.github.com>
2022-08-25 22:25:57 +0800

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,1 +1,19 @@
-
+public class Payment {
+    
+    protected String productName;
+    protected Double cost;
+  
+    public Product(String productName, Double cost) {
+        super();
+        this.productName = productName;
+        this.cost = cost;
+    }
+  
+    public String getProductName() {
+        return productName;
+    }
+ 
+    public Double cost() {
+        return cost;
+    }
+}
