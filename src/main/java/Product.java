3a6b0a353c53108da92bc7cc8e34c278e68fc506
KyawRP <22050023@myrp.edu.sg>
2022-08-28 11:54:44 +0800

    Attribute Name Changes Align to the Problem Statement

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,40 +1,42 @@
 
+
 public class Product {
 	
-	protected String productName;
-	protected String description;
-	protected String location;
-	protected String sellerName;
-	public Product(String productName, String description, String location, String sellerName) {
+	protected String product_name;
+	protected String product_country_of_origin;
+	protected String product_category;
+	protected String product_status;
+	
+	public Product(String product_name, String product_country_of_origin, String product_category, String sellerName) {
 		super();
-		this.productName = productName;
-		this.description = description;
-		this.location = location;
-		this.sellerName = sellerName;
+		this.product_name = product_name;
+		this.product_country_of_origin = product_country_of_origin;
+		this.product_category = product_category;
+		this.product_status = product_status;
 	}
 	public String getProductName() {
-		return productName;
+		return product_name;
 	}
-	public void setProductName(String productName) {
-		this.productName = productName;
+	public void setProductName(String product_name) {
+		this.product_name = product_name;
 	}
-	public String getDescription() {
-		return description;
+	public String getproduct_country_of_origin() {
+		return product_country_of_origin;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setDescription(String product_country_of_origin) {
+		this.product_country_of_origin = product_country_of_origin;
 	}
-	public String getLocation() {
-		return location;
+	public String getproduct_category() {
+		return product_category;
 	}
-	public void setLocation(String location) {
-		this.location = location;
+	public void setLocation(String product_category) {
+		this.product_category = product_category;
 	}
-	public String getSellerName() {
-		return sellerName;
+	public String getproduct_status() {
+		return product_status;
 	}
-	public void setSellerName(String sellerName) {
-		this.sellerName = sellerName;
+	public void setSellerName(String product_status) {
+		this.product_status = product_status;
 	}
 
 }
